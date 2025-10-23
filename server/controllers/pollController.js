532e948fe8d4af7e1dcf5d974b22cc44dd9a7273
Andrew Rafal <AndrewR270@users.noreply.github.com>
2025-10-22 20:15:43 -0700

    Workshop-2

--- server/controllers/pollController.js
+++ server/controllers/pollController.js
@@ -3,15 +3,62 @@ const Poll = require("../models/Poll");
 // TODO
 // ACTIVITY 2a - Implement getPolls function (return all polls)
 
+const getPolls = async() => {
+    const poll = await Poll.find();
+    console.log('Returning polls list...');
+    return poll;
+}
+
+
 // ACTIVITY 2b - Implement getPoll function (get one poll by id)
+
+const getPoll = async(id) => {
+    const poll = await Poll.findById(id);
+    console.log('Returning poll ${id}');
+    return poll;
+}
 
 // END ACTIVITY 2
 
 // TODO
 // ACTIVITY 3a - Implement postPoll function to create a new poll
 
+const postPoll = async ({ ownerId, title, description, options }) => {
+    if(!ownerId || !title || !options) return;
+
+    const poll = new Poll({
+        ownerId: ownerId,
+        title: title,
+        description: description,
+        options: options,
+    });
+
+    await poll.save();
+    return poll;
+}
+
 // ACTIVITY 3b - Implement postVote function to cast a vote
+
+const postVote = async ({ pollId, optionId }) => {
+    if (!pollId || !optionId) return;
+
+    const updateOption = await Poll.updateOne(
+        { _id: pollId, "options._id": optionId },
+        {
+            $inc: {"options.$.count": 1, totalVotes: 1 },
+        }
+    );
+
+    if(updateOption.modifiedCount == 0) return "Error: failed to retrieve poll";
+
+    const updatedPoll = await Poll.findById(pollId);
+
+    console.log('Vote cast for ${pollId} on option ${optionId}');
+    return updatedPoll;
+}
 
 // ACTIVITY 3c - Implement module exports
 
 // END ACTIVITY 3
+
+module.exports = { getPolls, getPoll, postPoll, postVote };
--- server/server.js
+++ server/server.js
@@ -17,3 +17,9 @@ server.use("/api", router);
 server.listen(config.PORT, () => {
   console.log("Server started listening on PORT " + config.PORT);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect(config.DB_URL)
+  .then (() => console.log('Connected to MongoDB'))
+  .catch ((err) => console.error('Error connecting to MongoDB: ', err));
